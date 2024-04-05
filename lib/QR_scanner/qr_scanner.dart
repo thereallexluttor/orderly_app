0338a722c0cab6cb652f51b6c0cec7bd76d2f8d0
Hedinyer Perucho <alucardperucho@gmail.com>
2024-04-06 01:13:22 +0200

    menu fixes

--- lib/QR_scanner/qr_scanner.dart
+++ lib/QR_scanner/qr_scanner.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: camel_case_types, use_super_parameters, non_constant_identifier_names, avoid_print, unused_element, prefer_interpolation_to_compose_strings, must_be_immutable, use_key_in_widget_constructors, prefer_const_constructors, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -13,6 +15,7 @@ class _QR_ScannerState extends State<QR_Scanner> {
   String result = "";
   String useruid = "";
   String photoUrl = "";
+  String ResDescription = "";
 
   @override
   void initState() {
@@ -24,7 +27,7 @@ class _QR_ScannerState extends State<QR_Scanner> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('QR Scanner'),
+        title: const Text('QR Scanner'),
       ),
       body: Center(
         child: Column(
@@ -52,7 +55,7 @@ class _QR_ScannerState extends State<QR_Scanner> {
       String itemDespuesDeMesas = parts[2];
       String RestaurantName = parts[3];
       _addNewFieldToMesa(rutaHastaMesas, itemDespuesDeMesas, useruid, photoUrl);
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MENU(result, photoUrl, rutaHastaMenu, RestaurantName)));
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MENU(result, photoUrl, rutaHastaMenu, RestaurantName,ResDescription)));
     });
   }
 
@@ -79,7 +82,7 @@ class _QR_ScannerState extends State<QR_Scanner> {
     int indexMesas = parts2.indexOf("mesas");
     String RestaurantName = parts2.sublist(indexRestaurants + 2, indexMesas).join(" ");
     String itemDespuesDeMesas = parts.length > 1 ? parts[1] : "";
-    return [rutaMenu, rutaHastaMesas, itemDespuesDeMesas, RestaurantName];
+    return [rutaMenu, rutaHastaMesas, itemDespuesDeMesas, RestaurantName, ResDescription];
   }
 }
 
@@ -88,8 +91,9 @@ class MENU extends StatelessWidget {
   final String scannedResult;
   final String photoUrl;
   final String RestaurantName;
+  String ResDescription;
 
-  MENU(this.scannedResult, this.photoUrl, this.MenuUrl, this.RestaurantName);
+   MENU(this.scannedResult, this.photoUrl, this.MenuUrl, this.RestaurantName, this.ResDescription);
 
   @override
   Widget build(BuildContext context) {
@@ -99,13 +103,22 @@ class MENU extends StatelessWidget {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Image(
-              image: AssetImage("lib/images/logos/orderly_icon3.png"),
-              height: 60,
-              width: 110,
+            InkWell(
+              onTap: () {
+                Navigator.pop(context); // Volver a la página anterior
+              },
+              child: const Icon(Icons.arrow_back), // Icono de flecha hacia atrás
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 17.0),
+              child: Image(
+                image: AssetImage("lib/images/logos/orderly_icon3.png"),
+                height: 50,
+                width: 80,
+              ),
             ),
             CircleAvatar(
-              backgroundImage: photoUrl.isNotEmpty ? NetworkImage(photoUrl) : AssetImage("lib/images/logos/default_avatar.png") as ImageProvider,
+              backgroundImage: photoUrl.isNotEmpty ? NetworkImage(photoUrl) : const AssetImage("lib/images/logos/default_avatar.png") as ImageProvider,
             ),
           ],
         ),
@@ -114,13 +127,11 @@ class MENU extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           SizedBox(
-            height: 150,
+            height: 250,
             child: Center(
               child: Card(
-                elevation: 8,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(70),
-                ),
+                elevation: 0,
+                
                 child: StreamBuilder(
                   stream: FirebaseFirestore.instance.collection('Orderly').doc('restaurantes').collection('restaurantes').doc(RestaurantName).snapshots(),
                   builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
@@ -131,38 +142,41 @@ class MENU extends StatelessWidget {
                     }
 
                     if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
+                      return const Center(
                         child: CircularProgressIndicator(),
                       );
                     }
 
                     final data = snapshot.data!.data() as Map<String, dynamic>;
                     final imageUrl = data['url'] as String;
+                    ResDescription = data['descripcion'] as String;
 
-                    return ClipOval(
-                      child: Image.network(
+                    return Image.network(
                         imageUrl,
-                        width: 150,
-                        height: 150,
-                        fit: BoxFit.cover,
-                      ),
-                    );
+                        width: 350,
+                        height: 250,
+                        fit: BoxFit.scaleDown,
+                      );
+
+                    
                   },
                 ),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(0.0),
             child: Text(
               RestaurantName,
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold, fontFamily: "Poppins-l"),
+              style: const TextStyle(fontSize: 17, fontWeight: FontWeight.normal, fontFamily: "Poppins-l"),
             ),
           ),
+
+          
           Expanded(
             child: StreamBuilder(
-              stream: FirebaseFirestore.instance.collection(MenuUrl).snapshots(),
+              stream: FirebaseFirestore.instance.collection(MenuUrl).orderBy('pos').snapshots(),
               builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
                 if (snapshot.hasError) {
                   return Center(
@@ -171,7 +185,7 @@ class MENU extends StatelessWidget {
                 }
 
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
+                  return const Center(
                     child: CircularProgressIndicator(),
                   );
                 }
@@ -180,14 +194,13 @@ class MENU extends StatelessWidget {
                 final tipoProductos = _extractTipoProductos(docs);
 
                 return SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
                     children: tipoProductos.map((tipoProducto){
                       final productos = _filterProductosByTipo(docs, tipoProducto);
                       return _buildTipoProductoColumn(tipoProducto, productos);
-                    }).toList()
-
-                  )
+                    }).toList(),
+                  ),
                 );
               },
             ),
@@ -197,53 +210,107 @@ class MENU extends StatelessWidget {
     );
   }
 
-  Widget _buildTipoProductoColumn(String tipoProducto, List<QueryDocumentSnapshot> productos) {
-    return SizedBox(
-      width: 300,
-      child: Card(
-        margin: EdgeInsets.all(10),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                tipoProducto,
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal, fontFamily: 'Poppins', color: Color.fromARGB(255, 193, 43, 212)),
-              ),
-              SizedBox(height: 10),
-              Column(
-                children: productos.map((producto) {
-                  return Card(
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: ListTile(
-                      leading: ClipRRect(
-                        borderRadius: BorderRadius.circular(8.0),
-                        child: Image.network(
-                          producto['url'] as String,
-                          width: 60,
-                          height: 60,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      title: Text(producto['NOMBRE_DEL_PRODUCTO'] as String, style: TextStyle(fontFamily: 'Poppins', fontSize: 13)),
-                      subtitle: Text('\$${producto['precio']}', style: TextStyle(fontFamily: 'Poppins', fontSize: 11)),
-                      onTap: () {
-                        // Implementa la lógica para manejar el tap en el producto si es necesario
-                      },
-                    ),
-                  );
-                }).toList(),
-              ),
-            ],
-          ),
+Widget _buildTipoProductoColumn(String tipoProducto, List<QueryDocumentSnapshot> productos) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          tipoProducto,
+          style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold, fontFamily: "Poppins-l", color: Color.fromARGB(255, 193, 43, 212)),
         ),
       ),
-    );
-  }
+      Column(
+        children: productos.map((producto) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey[300]!, width: 1.0)),
+              ),
+              child: Card(
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20.0),
+                              border: Border.all(color: Color.fromARGB(255, 235, 235, 235)),
+                            ),
+                            child: SizedBox(
+                              width: 180,
+                              height: 180,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20.0),
+                                child: Image.network(
+                                  producto['url'] as String,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Container(
+                            width: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  producto['NOMBRE_DEL_PRODUCTO'] as String,
+                                  style: const TextStyle(fontWeight: FontWeight.bold, fontFamily: "Poppins-l", fontSize: 11),
+                                ),
+                                SizedBox(height: 0),
+                                Container(
+                                  width: 180, // Establece el ancho máximo del texto
+                                  child: Text(
+                                    producto['descripcion'] as String,
+                                    style: const TextStyle(fontSize: 10, fontFamily: "Poppins-l", color: Colors.grey),
+                                    overflow: TextOverflow.ellipsis, // Trunca el texto si es demasiado largo
+                                    maxLines: 3, // Máximo de 3 líneas de texto
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 0),
+                          Text(
+                            '\$${producto['precio']}',
+                            style: const TextStyle(fontSize: 10, fontFamily: "Poppins-l", fontWeight: FontWeight.bold, color: Colors.purple),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      top: 8,
+                      right: 170,
+                      child: Container(
+                        padding: const EdgeInsets.all(4.0),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 249, 255, 248),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: Icon(Icons.add, color: Colors.green[800]),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    ],
+  );
+}
 
   List<String> _extractTipoProductos(List<QueryDocumentSnapshot> docs) {
     final tipoProductosSet = <String>{};
@@ -274,7 +341,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: QR_Scanner(),
+      home: const QR_Scanner(),
     );
   }
 }
